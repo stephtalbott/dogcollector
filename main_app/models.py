265de569ffe17b96ea55a39811410ae8965a859d
stephtalbott <talbottstephanie@ymail.com>
2023-05-31 16:11:56 -0500

    feeding functionality on detail page

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,12 @@
 from django.db import models
+from django.urls import reverse
+from datetime import date
+
+MEALS = (
+    ('B', 'Breakfast'), 
+    ('L', 'Lunch'), 
+    ('D', 'Dinner'),
+)
 
 # Create your models here.
 class Dog(models.Model):
@@ -9,3 +17,28 @@ class Dog(models.Model):
 
     def __str__(self):
         return f'{self.name} ({self.id})' 
+    
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'dog_id': self.id})
+    
+    def fed_for_today(self):
+        return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
+
+class Feeding(models.Model):
+    date = models.DateField('Feeding Date')
+    meal = models.CharField(
+        max_length=1, 
+        choices=MEALS, 
+        default=MEALS[0][0]
+    )
+    # create a dog_id FK
+    dog = models.ForeignKey(
+        Dog, 
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return f"{self.get_meal_display()} on {self.date}"
+    
+    class Meta:
+        ordering = ['-date']
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Dog
+from .forms import FeedingForm
 
 # define the home view 
 def home(request):
@@ -17,6 +19,30 @@ def dogs_index(request):
 
 def dogs_detail(request, dog_id):
     dog = Dog.objects.get(id=dog_id)
+    feeding_form = FeedingForm()
     return render(request, 'dogs/detail.html', {
-        'dog': dog
+        'dog': dog, 'feeding_form': feeding_form
     })
+
+class DogCreate(CreateView):
+    model = Dog
+    fields = '__all__' 
+
+class DogUpdate(UpdateView):
+    model = Dog
+    fields = ['breed', 'description', 'age']
+
+class DogDelete(DeleteView):
+    model = Dog
+    success_url = '/dogs'
+
+def add_feeding(request, dog_id):
+    form = FeedingForm(request.POST)
+    # validate the form 
+    if form.is_valid():
+        # don't save the form to the db without a dog_id assigned 
+        # a feeding has to belong to a dog!
+        new_feeding = form.save(commit=False)
+        new_feeding.dog_id = dog_id 
+        new_feeding.save()
+    return redirect('detail', dog_id=dog_id)
